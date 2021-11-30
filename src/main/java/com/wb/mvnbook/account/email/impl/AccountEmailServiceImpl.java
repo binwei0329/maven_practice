6dd5e3004d7ab6db5c5a5af713fde077dc8e322c
binwei0329 <williambinwei@hotmail.com>
2021-11-30 22:23:18 +0800

    编写AccountEmailService接口以及实现类

---
+++ src/main/java/com/wb/mvnbook/account/email/impl/AccountEmailServiceImpl.java
@@ -0,0 +1,45 @@
+package com.wb.mvnbook.account.email.impl;
+
+import com.wb.mvnbook.account.email.AccountEmailService;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+public class AccountEmailServiceImpl implements AccountEmailService {
+    private JavaMailSender javaMailSender;
+    private String systemEmail;
+
+    @Override
+    public void sendMail(String to, String subject, String htmlText) throws Exception {
+        try{
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            MimeMessageHelper msgHelper = new MimeMessageHelper(msg);
+            msgHelper.setFrom(systemEmail);
+            msgHelper.setTo(to);
+            msgHelper.setSubject(subject);
+            msgHelper.setText(htmlText, true);
+            javaMailSender.send(msg);
+
+        } catch (MessagingException ex){
+            throw new Exception("Failed to send email", ex);
+        }
+    }
+
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void setSystemEmail(String systemEmail) {
+        this.systemEmail = systemEmail;
+    }
+
+    public JavaMailSender getJavaMailSender() {
+        return javaMailSender;
+    }
+
+    public String getSystemEmail() {
+        return systemEmail;
+    }
+}
